0d85f26f5b908449cbad1a40fccbcab6cdc1b771
Matt Fergoda <mattfergoda@users.noreply.github.com>
2023-11-07 13:52:23 -0800

    Added app docstring.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import CardList from "./CardList";
 
+/** Renders App
+ *
+ * App -> CardList
+ */
+
 function App() {
   return (
     <div className="App">
